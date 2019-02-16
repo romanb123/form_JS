292c4706d4d98e437eeb48a03bd8f9e272f75d55
romanb123 <romanb123@users.noreply.github.com>
2019-02-16 17:38:55 +0200

    2 upd

---
+++ bootstrap-3.3.7-dist/js/form.js
@@ -0,0 +1,9 @@
+function shownum(){
+    var x=document.getElementById("showage");
+    if(x.checked==true){
+    document.getElementById("age").style.display="block";
+ }
+ else{
+    document.getElementById("age").style.display="none";    
+ }
+}
